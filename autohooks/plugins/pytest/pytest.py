e6624f863d16d748a58308df03f59eb45ce5b513
Björn Ricks <bjoern.ricks@greenbone.net>
2023-04-17 11:17:22 +0200

    Fix typo

--- autohooks/plugins/pytest/pytest.py
+++ autohooks/plugins/pytest/pytest.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Provides precommit function for pytest autohook plugin."""
+"""Provides precommit function for pytest autohooks plugin."""
 
 import subprocess
 import sys
